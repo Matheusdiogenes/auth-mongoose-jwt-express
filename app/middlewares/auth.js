25b6ff020921259e22f164ac851a667d8aa12a2a
Matheus diogenes <matheusdiogenes98@gmail.com>
2022-02-06 23:23:00 -0300

    Mudei a resposta do usuário não autorizado

--- app/middlewares/auth.js
+++ app/middlewares/auth.js
@@ -11,7 +11,7 @@ const verifyJWT = (req, res, next) => {
     if(!token) return res.status(401).send({ message: "No token provided." })
      
     jwt.verify(token, process.env.SECRET_KEY, (err , decoded) => {        
-        if(err) return res.status(401).send({ message: "Token invalid." }) 
+        if(err) return res.status(401).send({ message: "Token invalid/Unauthorized." }) 
         
         req.userID = decoded.id
         return next()
